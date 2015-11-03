dd1d66b1be044c8102684b31008944d5f45f16bf
ciction <swolfschristophe@gmail.com>
2015-11-03 18:04:24 +0100

    working scroll UI 1

--- EigenTests/src/SoftwareProject/ScrollableTest.java
+++ EigenTests/src/SoftwareProject/ScrollableTest.java
@@ -1,5 +1,6 @@
 package SoftwareProject;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,139 +14,209 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
 
+
+
 public class ScrollableTest {
 	
 	//Datamembers
 	//-------------------------
 	
-	//window
-	JFrame frame;	
-	//panels
-	JScrollPane myScrollPane;
-	JPanel panelInsideScrollPane;
-	//layout for panelInsideScrollPane
-	SpringLayout layout;
-	//components
+	//Window
+	JFrame frame;
+	private final int WINDOW_WIDTH = 800;
+	private final int WINDOW_HEIGHT = 600;
+	
+	
+	//Panels
+		//Scroll panel
+		JScrollPane myScrollPane;	
+		JScrollBar verticalScrollBar;
+		int verticalMargin = 5;
+	
+		//Inner content panel
+		JPanel panelInsideScrollPane;
+		//Layout for panelInsideScrollPane
+		SpringLayout layout;
+		
+	//Components
 	JButton buttonTest;	
+	CreateQuestionListener myListener;
+	
 	//used to store last created component;
 	JComponent CalculateNextOffsetFromThisComponent;
 	
-	JScrollBar verticalScrollBar;
-
-
+	//colors
+	Color[] colors = new Color[3]; 
+	Color currentColor;
 	
+
+	//----------------------------------------------------
+	
+	//getters setters
+	public JFrame getFrame() {
+		return frame;
+	}
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+	public JScrollPane getMyScrollPane() {
+		return myScrollPane;
+	}
+	public void setMyScrollPane(JScrollPane myScrollPane) {
+		this.myScrollPane = myScrollPane;
+	}
+	public JScrollBar getVerticalScrollBar() {
+		return verticalScrollBar;
+	}
+	public void setVerticalScrollBar(JScrollBar verticalScrollBar) {
+		this.verticalScrollBar = verticalScrollBar;
+	}
+	public int getVerticalMargin() {
+		return verticalMargin;
+	}
+	public void setVerticalMargin(int verticalMargin) {
+		this.verticalMargin = verticalMargin;
+	}
+	public JPanel getPanelInsideScrollPane() {
+		return panelInsideScrollPane;
+	}
+	public void setPanelInsideScrollPane(JPanel panelInsideScrollPane) {
+		this.panelInsideScrollPane = panelInsideScrollPane;
+	}
+	public SpringLayout getLayout() {
+		return layout;
+	}
+	public void setLayout(SpringLayout layout) {
+		this.layout = layout;
+	}
+	public JButton getButtonTest() {
+		return buttonTest;
+	}
+	public void setButtonTest(JButton buttonTest) {
+		this.buttonTest = buttonTest;
+	}
+	public CreateQuestionListener getMyListener() {
+		return myListener;
+	}
+	public void setMyListener(CreateQuestionListener myListener) {
+		this.myListener = myListener;
+	}
+	public JComponent getCalculateNextOffsetFromThisComponent() {
+		return CalculateNextOffsetFromThisComponent;
+	}
+	public void setCalculateNextOffsetFromThisComponent(
+			JComponent calculateNextOffsetFromThisComponent) {
+		CalculateNextOffsetFromThisComponent = calculateNextOffsetFromThisComponent;
+	}
+	public Color[] getColors() {
+		return colors;
+	}
+	public void setColors(Color[] colors) {
+		this.colors = colors;
+	}
+	public Color getCurrentColor() {
+		return currentColor;
+	}
+	public void setCurrentColor(Color currentColor) {
+		this.currentColor = currentColor;
+	}
 	
 	//----------------------------------------------------
+	
 	
 	//Constructor
 	public ScrollableTest() {
 		
-		//create window
+		//init color array
+		colors[0] = new Color(0x90C3D4);
+	    colors[1] = new Color(0x67AFC7); 
+	    colors[2] = new Color(0x75A1C7); 
+	    currentColor = colors[0];
+		
+		
+		//Create window
 		createWindow();
 		
-		//create panels
+		//Create panels
 		createPanels();
-		CalculateNextOffsetFromThisComponent = panelInsideScrollPane;
+		
+		//Add first question panels
+		addUIQuestion();
 	    
-		//set size for the pannels
-		panelInsideScrollPane.setPreferredSize(new Dimension(300,300));
-		myScrollPane.setPreferredSize(new Dimension(300,400));
-
+		//Set size for the panels
+		panelInsideScrollPane.setPreferredSize(new Dimension(WINDOW_WIDTH,WINDOW_HEIGHT));
+		myScrollPane.setPreferredSize(new Dimension(WINDOW_WIDTH,WINDOW_HEIGHT));
 	   
-		//add button
+		//Add button
 		buttonTest = new JButton("add component");
+		myListener = new CreateQuestionListener(this);
+		buttonTest.addActionListener(myListener);
 		panelInsideScrollPane.add(buttonTest);
 		
-		
-		layout.putConstraint(SpringLayout.SOUTH, buttonTest,-25,SpringLayout.SOUTH, CalculateNextOffsetFromThisComponent);
-		layout.putConstraint(SpringLayout.EAST, buttonTest,-25,SpringLayout.EAST, CalculateNextOffsetFromThisComponent);
+		//Place button bottom right
+		layout.putConstraint(SpringLayout.SOUTH, buttonTest,-25,SpringLayout.SOUTH, panelInsideScrollPane);
+		layout.putConstraint(SpringLayout.EAST, buttonTest,-25,SpringLayout.EAST, panelInsideScrollPane);
 
-		
-		//button interaction
-		buttonTest.addActionListener(new ActionListener() {
-			
-			//when clicked
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				//create new label
-				JLabel newElement = new JLabel("Bla");
-            	layout.putConstraint(SpringLayout.NORTH, newElement,25,SpringLayout.NORTH, CalculateNextOffsetFromThisComponent);
-            	CalculateNextOffsetFromThisComponent = newElement;
-            	
-            	
-            	//add label to the inner pannel (pannel within the scrollpane)
-               	panelInsideScrollPane.add(newElement);
-            	panelInsideScrollPane.validate();
-
-
-            	
-            	//resize if needed
-            	//debug numbers
-            	//System.out.println("bottom of new element = " + (newElement.getLocation().y + newElement.getHeight()));
-            	//System.out.println("top of button = " + buttonTest.getLocation().y);
-            	
-
-            	//current preferred panel size
-            	int x = (int) panelInsideScrollPane.getPreferredSize().getWidth();
-            	int y = (int) panelInsideScrollPane.getPreferredSize().getHeight();
-            	
-            	//if next elements is outside preferred bounds --> grow pannel
-            	if((newElement.getLocation().y + (newElement.getHeight() * 2)) >= (buttonTest.getLocation().y - 60))
-            	{
-            		panelInsideScrollPane.setPreferredSize(new Dimension(x,y + newElement.getHeight() + 10));
-            		if(verticalScrollBar != null ){
-            			verticalScrollBar.setValue( verticalScrollBar.getMaximum() );
-            		}
-            		else{
-            			verticalScrollBar = myScrollPane.getVerticalScrollBar();
-            		}
-            	}
-            
-            	frame.pack();
-				
-			}
-		});
-
-		
-
-
-	    
-	    
-	    //show window
+	    //Show window
 		frame.pack();
+		frame.setLocationRelativeTo(null);
 	    frame.setVisible(true);
-		
-		
+	    
 	}
+	
 	public void createWindow(){
 		
 		frame = new JFrame("Test");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(300, 400);
+	    frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 	
 	public void createPanels(){
-		//create scrollpane
+		//Create scrollpane
 	    myScrollPane = new JScrollPane();
 	    
-	    //create  panel with spring layout(for inside the scrollpane)
+	    //Create  panel with spring layout(for inside the scrollpane)
 	    panelInsideScrollPane = new JPanel();
 	    layout = new SpringLayout();
 		panelInsideScrollPane.setLayout(layout);
 	    
-		//add panel to the scrollpane
+		//Add panel to the scrollpane
 		myScrollPane.setViewportView(panelInsideScrollPane);
-		JScrollBar verticalScrollBar = myScrollPane.getVerticalScrollBar();
+		verticalScrollBar = new JScrollBar();
+		myScrollPane.setVerticalScrollBar(verticalScrollBar);
+
 		
-	    //add scrollpane to window
+		myScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		myScrollPane.getVerticalScrollBar().setUnitIncrement(32);
+		
+	    //Add scrollpane to window
 		frame.add(myScrollPane);
+		
 		
 	
 	}
 
+	public void addUIQuestion(){
+		//Create a first question panel
+		
+		JpanelTest newElement = new JpanelTest(WINDOW_WIDTH, currentColor);
+    	layout.putConstraint(SpringLayout.NORTH, newElement,5,SpringLayout.NORTH, panelInsideScrollPane);
+    	System.out.println("new element bounds =" + newElement.getBounds());
+
+      	
+    	
+      	panelInsideScrollPane.add(newElement);
+      	panelInsideScrollPane.doLayout();
+      	
+
+      	System.out.println("new element bounds =" + newElement.getBounds());
+      	
+      	
+      	
+     	CalculateNextOffsetFromThisComponent = newElement;
+	}
 	
-	
-	//entry point
+	//Entry point
 	public static void main(String args[]){
 		System.out.println("ScrollableTest start");
 		ScrollableTest test = new ScrollableTest();
